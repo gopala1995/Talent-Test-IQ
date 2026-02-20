9331d2b42d031c03ccc461d476279a48751e6bc2
Gopala <ggopala1904@gmail.com>
2026-02-20 14:05:22 +0530

    initial commit

---
+++ backend/src/lib/env.js
@@ -0,0 +1,8 @@
+import dotenv from "dotenv"
+
+dotenv.config()
+
+export const ENV = {
+    PORT: process.env.PORT,
+    DB_URl: process.env.DB_URl
+}
---
+++ backend/src/server.js
@@ -0,0 +1,11 @@
+import express from "express"
+import { ENV } from "./lib/env.js"
+
+const app = express()
+
+
+app.get("/health", (req, res) => {
+    res.status(200).json({ msg: "API is up and running" })
+})
+
+app.listen(ENV.PORT, () => console.log("listening port from ===>", ENV.PORT))
