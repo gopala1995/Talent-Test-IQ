aebc9b801d60dc1a166d987c890e026974b672ac
Gopala <ggopala1904@gmail.com>
2026-02-22 18:29:40 +0530

    commit !DB_URL condition

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -4,6 +4,9 @@ import { ENV } from "./env.js";
 
 export const connectionDB = async () => {
     try {
+        if (!ENV.DB_URL) {
+            throw new Error("MongoDB connection URL is not defined in environment variables.");
+        }
         const conn = await mongoose.connect(ENV.DB_URL)
         console.log("✅ Connected to MongoDB ✅", conn.connection.host);
 
