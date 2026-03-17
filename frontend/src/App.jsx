0d74521c41332fbb110117d08b0fba6a82d3cf52
Gopala <ggopala1904@gmail.com>
2026-03-17 10:19:52 +0530

    commit vercel json changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1>Gopala + React</h1>
     <SignedOut>
       <SignInButton mode="modal">
         <button >Log In</button>
