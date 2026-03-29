1f31d1dd73229e2eb03821c87ae53d439a887d26
Gopala <ggopala1904@gmail.com>
2026-03-29 10:40:11 +0530

    commit Landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,42 @@
 // import { useState } from 'react'
-import { SignedOut, SignedIn, SignInButton, SignOutButton, UserButton, useUser } from '@clerk/clerk-react'
-import { Navigate, Route, Routes } from 'react-router'
-import HomePage from './pages/HomePage'
-import ProblemsPage from './pages/ProblemsPage'
-import { Toaster } from 'react-hot-toast'
+import {
+  SignedOut,
+  SignedIn,
+  SignInButton,
+  SignOutButton,
+  UserButton,
+  useUser,
+} from "@clerk/clerk-react";
+import { Navigate, Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+import ProblemsPage from "./pages/ProblemsPage";
+import { Toaster } from "react-hot-toast";
+import DashboardPage from "./pages/DashboardPage";
 
 function App() {
-  // const [count, setCount] = useState(0)
-  const {isSignedIn} = useUser()
+  const { isSignedIn, isLoaded } = useUser();
+
+  if (!isLoaded) return null;
 
   return (
     <>
-    <Routes>
-      <Route path='/' element={<HomePage />} />
-      <Route path='/problems' element={isSignedIn? <ProblemsPage /> : <Navigate to="/"/>} />
-    </Routes>
-    <Toaster  toastOptions={{duration:3000}}/>
+      <Routes>
+        <Route
+          path="/"
+          element={!isSignedIn ? <HomePage /> : <Navigate to="/dashboard" />}
+        />
+        <Route
+          path="/dashboard"
+          element={isSignedIn ? <DashboardPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/problems"
+          element={isSignedIn ? <ProblemsPage /> : <Navigate to="/" />}
+        />
+      </Routes>
+      <Toaster toastOptions={{ duration: 3000 }} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
