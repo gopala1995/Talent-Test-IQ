c7a5ff20723120a445df8b2b2361d18bc8f5dc8a
Gopala <ggopala1904@gmail.com>
2026-04-07 14:46:35 +0530

    commit inngest fix

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -3,7 +3,7 @@ import { connectionDB } from "./db.js";
 import User from "../models/User.js";
 import { deleteStreamUser, upsertStreamUser } from "./stream.js";
 
-export const inngest = new Inngest({ id: "talent-iq" });
+export const inngest = new Inngest({ id: "talent-test-iq" });
 
 const syncUser = inngest.createFunction(
     { id: "sync-user" },
