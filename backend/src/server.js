472b6851db21ee2aa4e3ed1fda0ed704fb75ffb5
Gopala <ggopala1904@gmail.com>
2026-04-07 13:44:32 +0530

    dashboard page created

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,42 +1,42 @@
-import express from "express"
-import path from "path"
-import cors from "cors"
-import { serve } from "inngest/express"
-import { clerkMiddleware } from '@clerk/express'
+import express from "express";
+import path from "path";
+import cors from "cors";
+import { serve } from "inngest/express";
+import { clerkMiddleware } from "@clerk/express";
 
-import { ENV } from "./lib/env.js"
-import { connectionDB } from "./lib/db.js"
-import { inngest, functions } from "./lib/inngest.js"
-import chatRoutes from "./routes/chatRoutes.js"
-import sessionRoutes from "./routes/sessionRoutes.js"
+import { ENV } from "./lib/env.js";
+import { connectionDB } from "./lib/db.js";
+import { inngest, functions } from "./lib/inngest.js";
 
-const app = express()
+import chatRoutes from "./routes/chatRoutes.js";
+import sessionRoutes from "./routes/sessionRoutes.js";
 
-const __dirname = path.resolve()
+const app = express();
 
-app.use(express.json())
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }))
-app.use(clerkMiddleware()) // Add Clerk middleware to handle authentication
+const __dirname = path.resolve();
 
-app.use("/api/inngest", serve({ client: inngest, functions }))
-app.use("/api/chats", chatRoutes)
-app.use("/api/sessions", sessionRoutes)
+app.use(express.json());
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(clerkMiddleware()); // Add Clerk middleware to handle authentication
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/chat", chatRoutes);
+app.use("/api/sessions", sessionRoutes);
 
 
 
 app.get("/health", (req, res) => {
-    res.status(200).json({ msg: "API is up and running" })
-})
-
+  res.status(200).json({ msg: "api is up and running" });
+});
 
 
 //our app is ready to deployment from here only
-
 if (ENV.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")))
-    app.get("/{*any}", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
-    })
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
 
